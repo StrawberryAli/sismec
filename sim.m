98710ea97e729be8defda6486ff179d17681ad05
China <yukio.yamada2022@aluno.ufsj.edu.br>
2026-07-08 20:17:24 +0000

    adicionado força e solver

--- sim.m
+++ sim.m
@@ -1,20 +1,11 @@
 clear all;
 close all;
-counter=1;
-
-massa = 101.9368;
-damper = 0;
-%spring= 1e5;
-frequencia = 25.13;
-%v_ini = 1.42;
-
-for spring=1000:1000:5000.0
-    sim("model.slx");
-    plot((ans.t),ans.x_t, 'LineWidth', 2);
-    hold on;
-    leg(counter)=sprintf('spring= %.2f', spring);
-    counter=counter+1;
-end
+spring=40100;
+sim("model.slx");
+plot((ans.t),ans.x_t, 'LineWidth', 2);
+hold on;
+leg(counter)=sprintf('spring= %.2f', spring);
+counter=counter+1;
 
 legend(leg);
 grid on;
